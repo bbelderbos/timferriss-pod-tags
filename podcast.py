5070df97c65174ede9362b77248bdca059840626
Bob Belderbos <bobbelderbos@gmail.com>
2022-04-25 20:04:02 +0200

    using feedparser

--- podcast.py
+++ podcast.py
@@ -5,14 +5,27 @@ https://github.com/bbelderbos/Codesnippets/blob/master/python/ferriss_podcast_ta
 from collections import Counter
 import re
 
+import feedparser
 import requests
 
+AUTHOR = "ferris"  # sometimes it gets misspelled
 TAG_REGEX = re.compile(r'tag-[^" ]+')
 
 
-def parse_feed(url):
+def parse_html_page(url):
     resp = requests.get(url)
     tags = re.findall(TAG_REGEX, resp.text)
+    return Counter(tags)
+
+
+def parse_feed(url):
+    ret = feedparser.parse(url)
+    tags = []
+    for entry in ret.entries:
+        tags.extend(
+            [t.term.lower() for t in entry.tags
+             if AUTHOR not in t.term.lower()]
+        )
     return Counter(tags)
 
 
@@ -23,8 +36,9 @@ def show_results(tags, min_tags=1):
 
 
 def main():
-    podcast_url = "http://fourhourworkweek.com/podcast/"
-    tags = parse_feed(podcast_url)
+    url = "http://fourhourworkweek.com/podcast/"
+    feed = "https://rss.art19.com/tim-ferriss-show"
+    tags = parse_feed(feed)
     show_results(tags)
 
 
