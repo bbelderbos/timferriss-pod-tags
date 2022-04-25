b3b1e7deea097bcaac09c939dd6ee46317b95937
Bob Belderbos <bobbelderbos@gmail.com>
2022-04-25 19:33:36 +0200

    findall

--- podcast.py
+++ podcast.py
@@ -1,39 +1,24 @@
 """
-Script from
+Original:
 https://github.com/bbelderbos/Codesnippets/blob/master/python/ferriss_podcast_tags.py
 """
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from collections import Counter
 import re
-from urllib.request import urlopen
 
-import bs4
 import requests
 
-podcast_url = "http://fourhourworkweek.com/podcast/"
-TAG_REXEG = re.compile(r'tag-[^" ]+')
+TAG_REGEX = re.compile(r'tag-[^" ]+')
 
 
 def parse_feed(url):
-    """
-    http://stackoverflow.com/questions/6213063/python-read-next
-    """
     resp = requests.get(url)
-    res = []
-    for line in resp.text.splitlines():
-        if 'tag-' not in line:
-            continue
-        mtags = TAG_REXEG.findall(line)
-        tags = [tag.replace("tag-", " ") for tag in mtags]
-        res.extend(tags)
-    ret = Counter(res)
-    return ret
+    tags = re.findall(TAG_REGEX, resp.text)
+    return Counter(tags)
 
 
 def main():
+    podcast_url = "http://fourhourworkweek.com/podcast/"
     res = parse_feed(podcast_url)
-
     for tag, count in res.most_common():
         if count > 1:
             print(f"{tag:30} | {count}")
