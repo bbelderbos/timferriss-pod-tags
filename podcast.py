5d710b0789ef8dd6a1c36d3ef2c974aac09c1678
Bob Belderbos <bobbelderbos@gmail.com>
2022-04-26 20:05:41 +0200

    live coded solution

--- podcast.py
+++ podcast.py
@@ -1,54 +1,48 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import urllib2
+from collections import Counter
 import re
 
-filename = "index.html"
-podcast_url = "http://fourhourworkweek.com/podcast/"
+import feedparser
+import requests
 
-# http://stackoverflow.com/questions/24346872/python-equivalent-of-a-given-wget-command
-def get_index():
-  attempts = 0
-  while attempts < 3:
-    try:
-      response = urllib2.urlopen(podcast_url, timeout = 5)
-      content = response.read()
-      f = open( filename, 'w' )
-      f.write( content )
-      f.close()
-      break
-    except urllib2.URLError as e:
-      attempts += 1
-      print type(e)
+from decorators import retry
 
-def parse_feed():
-  res = {}
-  # http://stackoverflow.com/questions/6213063/python-read-next
-  with open(filename, 'r+') as f:
-    for line in f:
-      if not line.strip():
-        continue
-      if 'id="post-' in line: 
-        mtags = tag.findall(line)
-        tags = [t.lstrip("tag").replace("-", " ") for t in mtags]
-        for t in tags:
-          if "ferris" in t or "podcast" in t or "show" in t:
-            continue
-          if t not in res:
-            res[t] = 0
-          res[t] += 1
-  return res
+FERRISS = "ferris"  # sometimes get it wrong
+
+@retry
+def get_podcast_html(url: str) -> str:
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
+
+
+def parse_html(content: str) -> Counter:
+    return Counter(
+        tag.replace("tag-", "") for tag in
+        re.findall(r'tag-[^" ]+', content)
+    )
+
+
+def parse_feed(feed: str) -> list[str]:
+    tags = []
+    entries = feedparser.parse(feed).entries
+    for entry in entries:
+        tags.extend(
+            [tag.term.lower() for tag in entry.tags
+             if FERRISS not in tag.term.lower()]
+        )
+    return tags
+
+
+def show_results(tags: Counter, min_count: int = 20) -> None:
+    for tag, count in tags.most_common():
+        if count > min_count:
+            print(f"{tag:30} | {count}")
+
 
 if __name__ == "__main__":
-  get_index()
-  tag = re.compile(r'tag-[^" ]+')
-  date = re.compile(r'201\d/\d{2}/\d{2}')
-  res = parse_feed()
-
-  order = []
-  for t, tot in res.items():
-    order.append( (tot, "%-50s | %s" % (t, "+"*tot) ) )
-
-  for (tot, line) in reversed(sorted(order)):
-    if tot > 2:
-      print line
+    podcast_url = "http://fourhourworkweek.com/podcast/"
+    # content = get_podcast_html(podcast_url)
+    # tags = parse_html(content)
+    feed = "https://rss.art19.com/tim-ferriss-show"
+    tags = Counter(parse_feed(feed))
+    show_results(tags)
